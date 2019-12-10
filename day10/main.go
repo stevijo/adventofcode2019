48910e2f212b7b72537e5651ea1047aa56475cd0
Stefan Mayer <stevijo.mayer@googlemail.com>
2019-12-11 00:33:24 +0100

    day10 optimize

---
+++ day10/main.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"sort"
+)
+
+var inputFile string
+
+func init() {
+	flag.StringVar(&inputFile, "input", "", "Asteroid map file")
+}
+
+func main() {
+	flag.Parse()
+	if inputFile == "" {
+		flag.PrintDefaults()
+		return
+	}
+
+	file, err := os.Open(inputFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	asteroidMap := [][]byte{}
+
+	for scanner.Scan() {
+		asteroidMap = append(asteroidMap, []byte(scanner.Text()))
+	}
+
+	var highestAmount int
+	var angleMap AngleMap
+	for i := 0; i < len(asteroidMap); i++ {
+		for j := 0; j < len(asteroidMap[i]); j++ {
+			if asteroidMap[i][j] != '#' {
+				continue
+			}
+			newAngleMap := generateAngleMap(j, i, asteroidMap)
+			if newAngleMap.ObjectsInSight() > highestAmount {
+				angleMap = newAngleMap
+				highestAmount = angleMap.ObjectsInSight()
+			}
+		}
+	}
+
+	fmt.Printf("Part 1: %v\n", highestAmount)
+	coord := vaporizeAsteroids(angleMap)
+	fmt.Printf("Part 2: %v\n", coord.X*100+coord.Y)
+}
+
+func vaporizeAsteroids(angleMap AngleMap) (coord Coordinate) {
+	const MAX_COUNT = 200
+	var (
+		destroyedAsteroids uint
+		keys               = make([]float64, 0, len(angleMap))
+	)
+
+	for key, _ := range angleMap {
+		keys = append(keys, key)
+	}
+	sort.Sort(sort.Float64Slice(keys))
+
+	for destroyedAsteroids < MAX_COUNT {
+		for _, idx := range keys {
+			coords := angleMap[idx]
+			if len(coords) > 0 {
+				destroyedAsteroids++
+				if destroyedAsteroids == MAX_COUNT {
+					coord = coords[0]
+				}
+
+				angleMap[idx] = coords[1:]
+			}
+		}
+	}
+	return coord
+}
+
+type AngleMap map[float64][]Coordinate
+
+func (a AngleMap) ObjectsInSight() int {
+	return len(a)
+}
+
+func generateAngleMap(x, y int, asteroidMap [][]byte) AngleMap {
+	var (
+		width    = len(asteroidMap[0])
+		height   = len(asteroidMap)
+		angleMap = make(map[float64][]Coordinate)
+	)
+
+	for i := 0; i < width; i++ {
+		for j := 0; j < height; j++ {
+			if j == y && i == x || asteroidMap[j][i] != '#' {
+				continue
+			}
+
+			var (
+				dx    = float64(x - i)
+				dy    = float64(y - j)
+				angle = math.Atan2(dy, dx) - math.Pi/2
+			)
+			if angle < 0 {
+				angle += math.Pi * 2
+			}
+
+			angleMap[angle] = append(angleMap[angle], Coordinate{
+				i, j,
+			})
+			sort.Slice(angleMap[angle], func(i, j int) bool {
+				return angleMap[angle][i].distance(x, y) < angleMap[angle][j].distance(x, y)
+			})
+		}
+	}
+
+	return angleMap
+}
+
+type Coordinate struct {
+	X, Y int
+}
+
+func (c *Coordinate) distance(x, y int) uint {
+	return uint(math.Abs(float64(x-c.X)) + math.Abs((float64(y - c.Y))))
+}
--- day9/main.go
+++ day9/main.go
@@ -5,15 +5,17 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"math"
 	"os"
-	"sort"
+
+	"github.com/stevijo/adventofcode2019/common/machine"
 )
 
-var inputFile string
+var (
+	inputFile string
+)
 
 func init() {
-	flag.StringVar(&inputFile, "input", "", "Asteroid map file")
+	flag.StringVar(&inputFile, "input", "", "Input file for advent of code.")
 }
 
 func main() {
@@ -29,174 +31,32 @@ func main() {
 	}
 
 	scanner := bufio.NewScanner(file)
-	asteroidMap := [][]byte{}
+	scanner.Scan()
 
-	for scanner.Scan() {
-		asteroidMap = append(asteroidMap, []byte(scanner.Text()))
-	}
-	var highestAmount int
-	var coordinates struct{ x, y int }
-	for i := 0; i < len(asteroidMap); i++ {
-		for j := 0; j < len(asteroidMap[i]); j++ {
-			if asteroidMap[i][j] != '#' {
-				continue
-			}
-			amount := sightCount(j, i, asteroidMap)
-			if amount > highestAmount {
-				coordinates = struct {
-					x int
-					y int
-				}{j, i}
-				highestAmount = amount
+	data := scanner.Text()
 
-			}
-		}
-	}
-
-	fmt.Println(highestAmount)
-	fmt.Println(coordinates)
-	fmt.Println(vaporizeAsteroids(coordinates.x, coordinates.y, asteroidMap))
+	part1(data)
+	part2(data)
 }
 
-func vaporizeAsteroids(x, y int, asteroidMap [][]byte) Coordinate {
-	var i uint
-	var noOfAsteroids int
-	for {
-		quadrant := Quadrant(i % 4)
-		asteroids := checkQuadrant(quadrant, x, y, asteroidMap, i%2 == 0)
-
-		if noOfAsteroids+len(asteroids) >= 200 {
-			sort.Sort(SortCoordinate(asteroids))
-			diff := 200 - noOfAsteroids - 1
-			return asteroids[diff]
-		} else {
-			// change map
-			for _, asteroid := range asteroids {
-				// vaporize
-				asteroidMap[asteroid.Y][asteroid.X] = '.'
-			}
-			noOfAsteroids += len(asteroids)
-		}
-		i++
-	}
+func part1(data string) {
+	output := make(chan int, 1)
+	input := make(chan int, 2)
+	input <- 1
+	intComputer := machine.NewMachine(data)
+	intComputer.SetInput(input)
+	intComputer.SetOutput(output)
+	<-intComputer.RunMachine()
+	fmt.Printf("Part1: %v\n", <-output)
 }
 
-func sightCount(x, y int, asteroidMap [][]byte) int {
-	return len(checkQuadrant(TopLeft, x, y, asteroidMap, false)) + len(checkQuadrant(TopRight, x, y, asteroidMap, true)) +
-		len(checkQuadrant(BottomRight, x, y, asteroidMap, false)) + len(checkQuadrant(BottomLeft, x, y, asteroidMap, true))
-}
-
-type Quadrant uint
-
-const (
-	TopRight Quadrant = iota
-	BottomRight
-	BottomLeft
-	TopLeft
-)
-
-var (
-	_ sort.Interface = SortCoordinate{}
-)
-
-type Coordinate struct {
-	X, Y  int
-	angle float64
-}
-
-type SortCoordinate []Coordinate
-
-func (sort SortCoordinate) Len() int {
-	return len(sort)
-}
-
-func (sort SortCoordinate) Less(i, j int) bool {
-	return sort[i].angle < sort[j].angle
-}
-
-func (sort SortCoordinate) Swap(i, j int) {
-	tmp := sort[i]
-	sort[i] = sort[j]
-	sort[j] = tmp
-}
-
-func checkQuadrant(quadrand Quadrant, x, y int, asteroidMap [][]byte, inclusive bool) []Coordinate {
-	var (
-		xFactor, yFactor int
-		i                = x
-		j                = y
-	)
-
-	switch quadrand {
-	case TopLeft:
-		xFactor = -1
-		yFactor = -1
-		break
-	case TopRight:
-		xFactor = 1
-		yFactor = -1
-		break
-	case BottomLeft:
-		xFactor = -1
-		yFactor = 1
-		break
-	case BottomRight:
-		xFactor = 1
-		yFactor = 1
-		break
-	}
-	if !inclusive {
-		i += xFactor
-		j += yFactor
-	}
-
-	coordinates := make([]Coordinate, 0)
-
-	blockedAngles := make(map[float64]struct{}, 0)
-	for {
-		if xFactor == -1 && i < 0 || xFactor == 1 && i >= len(asteroidMap[0]) {
-			break
-		}
-
-		for {
-			if j == y && i == x {
-				j += yFactor
-				continue
-			}
-
-			if yFactor == -1 && j < 0 || yFactor == 1 && j >= len(asteroidMap) {
-				break
-			}
-
-			if asteroidMap[j][i] != '#' {
-				j += yFactor
-				continue
-			}
-
-			angle := math.Abs(float64(j-y)) / math.Abs(float64(i-x))
-			if yFactor == 1 {
-				angle = 1 / angle
-			}
-			if _, ok := blockedAngles[angle]; ok {
-				j += yFactor
-				continue
-			}
-
-			blockedAngles[angle] = struct{}{}
-			coordinates = append(coordinates, Coordinate{
-				X:     i,
-				Y:     j,
-				angle: angle,
-			})
-			j += yFactor
-		}
-
-		j = y
-		if !inclusive {
-			j += yFactor
-		}
-		i += xFactor
-	}
-
-	return coordinates
+func part2(data string) {
+	output := make(chan int, 1)
+	input := make(chan int, 2)
+	input <- 2
+	intComputer := machine.NewMachine(data)
+	intComputer.SetInput(input)
+	intComputer.SetOutput(output)
+	<-intComputer.RunMachine()
+	fmt.Printf("Part2: %v\n", <-output)
 }
